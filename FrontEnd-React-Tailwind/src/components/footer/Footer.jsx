c6728033464e34b7bf844745b4ede7f41044936b
AgustinJuarez96 <juarezagustin1996@gmail.com>
2023-10-18 20:08:06 -0300

    footer

--- FrontEnd-React-Tailwind/src/components/footer/Footer.jsx
+++ FrontEnd-React-Tailwind/src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
@@ -7,12 +8,25 @@ function Footer() {
         <p className="text-lg font-semibold">¡Gracias por visitar nuestro sitio web!</p>
         <p className="text-sm">© 2023 Todos los derechos reservados</p>
       </div>
+      <div class="footer-icons">
+    <a href="https://www.facebook.com" class="icon-container">
+        <i class="fab fa-facebook-f"></i>
+    </a>
+    <a href="https://www.instagram.com" class="icon-container">
+        <i class="fab fa-instagram"></i>
+    </a>
+    <a href="https://www.tiktok.com" class="icon-container">
+        <i class="fab fa-tiktok"></i>
+    </a>
+    <a href="https://www.twitter.com" class="icon-container">
+        <i class="fab fa-twitter"></i>
+    </a>
+</div>
     </footer>
   );
 }
 
 export default Footer;
-
 
 
 
