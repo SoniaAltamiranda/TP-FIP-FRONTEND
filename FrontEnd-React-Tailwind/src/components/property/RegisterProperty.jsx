3f3aed663f67a81fb6584674f95d3b39e67782a2
Eric Schenone <ericschenone90@gmail.com>
2023-11-16 15:41:32 -0300

    Revert "Componente RegisterProperty con drag and drop" Faltaron cambios en el componente. This reverts commit 2c73472f1a015d5365ba693ed6ab3f068565f9e6.

--- FrontEnd-React-Tailwind/src/components/property/RegisterProperty.jsx
+++ FrontEnd-React-Tailwind/src/components/property/RegisterProperty.jsx
@@ -1,14 +1,9 @@
 import { useState } from "react";
 import Swal from "sweetalert2";
-import { useContext } from "react";
-import { usersContext } from "../../context/usersContext";
 
 const BASE_URL = "http://localhost:3000/properties/";
 
 const RegisterProperty = () => {
-
-  const { currentUser } = useContext(usersContext);
-
   const initialFormData = {
     title: "",
     type: "",
@@ -16,14 +11,19 @@ const RegisterProperty = () => {
     rooms: "",
     description: "",
     price: "",
-    rate: 0,
     images: [],
-    id_propietor: "",
-    url_iframe: "",
   };
 
   const [formData, setFormData] = useState(initialFormData);
 
+  const handleImageChange = (e) => {
+    const { value } = e.target;
+    const imageUrls = value.split("\n").filter((url) => url.trim() !== "");
+    setFormData({
+      ...formData,
+      images: imageUrls,
+    });
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -33,14 +33,8 @@ const RegisterProperty = () => {
     });
   };
 
-  const id_user = currentUser ? currentUser.id : null;
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!id_user) {
-      alert("Debes iniciar sesión para publicar una propiedad.");
-      return;
-    }
-
     const jsonData = {
       title: formData.title,
       type: formData.type,
@@ -48,11 +42,7 @@ const RegisterProperty = () => {
       rooms: parseInt(formData.rooms),
       description: formData.description,
       price: parseInt(formData.price),
-      rate: 0,//provisoriamente
       images: formData.images,
-      id_propietor: id_user,
-      url_iframe: "http......",//provisoriamente para poder mostrar
-
     };
 
     fetch(BASE_URL, {
@@ -80,7 +70,7 @@ const RegisterProperty = () => {
           confirmButtonText: "Continuar",
         }).then((result) => {
           if (result.isConfirmed) {
-          location.reload();
+            window.location.href = "http://localhost:5173/user";
           }
         });
         setFormData(initialFormData);
@@ -97,92 +87,42 @@ const RegisterProperty = () => {
       });
   };
 
-  // Función para prevenir el comportamiento predeterminado de arrastrar y soltar
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  // Función para manejar la caída de archivos
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const files = e.dataTransfer.files;
-    const imageUrls = formData.images.slice(); // Copia la lista de imágenes actual
-
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const imageUrl = URL.createObjectURL(file); // Genera una URL temporal para la imagen
-      imageUrls.push(imageUrl);
-    }
-
-    setFormData({
-      ...formData,
-      images: imageUrls,
-    });
-  };
-
-  // Función para seleccionar archivos 
-  const handleFileSelect = (e) => {
-    const files = e.target.files;
-    const imageUrls = formData.images.slice(); // Copia la lista de imágenes actual
-
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const imageUrl = URL.createObjectURL(file); // Genera una URL temporal para la imagen
-      imageUrls.push(imageUrl);
-    }
-
-    setFormData({
-      ...formData,
-      images: imageUrls,
-    });
-  };
-
-  const handleRemoveImage = (index) => {
-    // Copia el array de imágenes actual y elimina la imagen en el índice proporcionado
-    const updatedImages = [...formData.images];
-    updatedImages.splice(index, 1);
-  
-    // Actualiza el estado con las imágenes actualizadas
-    setFormData({ ...formData, images: updatedImages });
-  };
-
-
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen mt-10">
       <div className="w-3/4 p-2 bg-white rounded-lg shadow-md">
-        <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+  <label htmlFor="title" className="block text-gray-800 font-bold mb-1">
+    Título:
+  </label>
+  <input
+    type="text"
+    id="title"
+    name="title"
+    value={formData.title}
+    onChange={handleInputChange}
+    className="w-full px-3 py-1 border rounded-lg focus:outline-none focus:shadow-outline"
+    required
+    pattern="[A-Za-záéíóúÁÉÍÓÚñÑ\s]+"
+    title="Solo se permiten letras y espacios en blanco"
+  />
+</div>
           <div className="mb-2">
-            <label htmlFor="title" className="block text-gray-800 font-bold mb-1">
-              Título:
+            <label
+              htmlFor="title"
+              className="block text-gray-800 font-bold mb-1"
+            >
+              Tipo de Alquiler:
             </label>
             <input
               type="text"
-              id="title"
-              name="title"
-              value={formData.title}
-              onChange={handleInputChange}
-              className="w-full px-3 py-1 border rounded-lg focus:outline-none focus:shadow-outline"
-              required
-              pattern="[A-Za-záéíóúÁÉÍÓÚñÑ\s]+"
-              title="Solo se permiten letras y espacios en blanco"
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="type" className="block text-gray-800 font-bold mb-1">
-              Tipo de Alquiler:
-            </label>
-            <select
               id="type"
               name="type"
               value={formData.type}
               onChange={handleInputChange}
               className="w-full px-3 py-1 border rounded-lg focus:outline-none focus:shadow-outline"
               required
-            >
-              <option value="">Seleccionar tipo de alquiler</option>
-              <option value="Alquiler temporal">Alquiler temporal</option>
-              <option value="Alquiler a largo plazo">Alquiler a largo plazo</option>
-            </select>
+            />
           </div>
           <div className="mb-2">
             <label
@@ -225,12 +165,13 @@ const RegisterProperty = () => {
             >
               Descripcion:
             </label>
-            <textarea
+            <input
+              type="text"
               id="description"
               name="description"
               value={formData.description}
               onChange={handleInputChange}
-              className="w-full px-3 py-1 border rounded-lg focus:outline-none focus:shadow-outline h-auto resize-none block"
+              className="w-full px-3 py-1 border rounded-lg focus:outline-none focus:shadow-outline"
               required
             />
           </div>
@@ -239,7 +180,7 @@ const RegisterProperty = () => {
               htmlFor="title"
               className="block text-gray-800 font-bold mb-1"
             >
-              Precio - $:
+              Precio:
             </label>
             <input
               type="number"
@@ -251,45 +192,23 @@ const RegisterProperty = () => {
               required
             />
           </div>
-
           <div className="mb-2">
-            <label htmlFor="imageUpload" className="block text-gray-800 font-bold mb-2">
-              Imágenes:
+            <label
+              htmlFor="imageUrls"
+              className="block text-gray-800 font-bold mb-2"
+            >
+              URLs de las Imágenes (una por línea):
             </label>
-            <div
-              id="imageDropArea"
-              className="w-full px-3 py-3 border rounded-lg focus:outline-none focus:shadow-outline transition-colors ease-in-out duration-300 hover:bg-blue-200"
-              onDragOver={(e) => handleDragOver(e)}
-              onDrop={(e) => handleDrop(e)}
-            ><label htmlFor="imageUpload">
-              Arrastra y suelta imágenes aquí o haz clic para seleccionarlas.
-            </label></div>
-            <input
-              type="file"
-              id="imageUpload"
-              accept="image/*"
-              style={{ display: "none" }}
-              onChange={(e) => handleFileSelect(e)}
+            <textarea
+              id="imageUrls"
+              name="imageUrls"
+              value={formData.images.join("\n")}
+              onChange={handleImageChange}
+              className="w-full px-3 py-1 border rounded-lg focus:outline-none focus:shadow-outline"
+              rows="3"
+              required
             />
           </div>
-          {formData.images.length > 0 && (
-            <div className="mb-2">
-            <label className="block text-gray-800 font-bold mb-2">Imágenes cargadas:</label>
-            <div className="flex flex-wrap">
-              {formData.images.map((imageUrl, index) => (
-                <div key={index} className="relative w-1/4 p-2">
-                  <img src={imageUrl} alt={`Imagen ${index + 1}`} className="w-full h-auto rounded" />
-                  <button
-                    className="absolute top-0 right-0 p-1 text-red-500 hover:text-red-700 cursor-pointer"
-                    onClick={() => handleRemoveImage(index)}
-                  >
-                    <img src="../public/images/boton-x.png" alt="Eliminar" className="w-4 h-4" />
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-          )}
           <div className="text-center">
             <button
               type="submit"
